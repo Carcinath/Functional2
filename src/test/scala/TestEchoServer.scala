48a729753814d32ae73f5c88e2e2b5d9eacaa535
Christopher Menefee <31318316+Carcinath@users.noreply.github.com>
2019-04-04 14:21:51 -0600

    Add two tests to the test file

--- src/test/scala/TestEchoServer.scala
+++ src/test/scala/TestEchoServer.scala
@@ -1,8 +1,9 @@
-import java.io.{BufferedReader, BufferedWriter, ByteArrayInputStream, ByteArrayOutputStream}
+import java.io._
 import java.net._
 
 import org.scalatest._
 import org.mockito.Mockito._
+import org.mockito.Matchers._
 import org.scalatest.mockito.MockitoSugar
 
 class TestEchoServer extends FlatSpec with Matchers with MockitoSugar {
@@ -34,5 +35,36 @@ class TestEchoServer extends FlatSpec with Matchers with MockitoSugar {
     verify(out).flush()
     verify(out).close()
     verify(in).close()
+  }
+
+  "Error Handling" should "echo" in {
+    val in = mock[String]
+    val out = mock[BufferedWriter]
+
+    when(in.split(" ").last).thenReturn("This is a test")
+
+    verify(out).write("This is a test \r\n")
+    verify(out).write("Still a test\r\n")
+    verify(out).write("Nine out of ten mothers agree this is a test")
+    verify(out).write("\r\n")
+
+    verify(out).flush()
+    verify(out).close()
+
+  }
+
+  "Read Home" should "echo" in{
+    val in = mock[String]
+    val out = mock[BufferedWriter]
+
+    when(in.split(" ").last).thenReturn("This is a Test")
+
+    verify(out).write("We're not sure what the tenth mother thinks. \r\n")
+    verify(out).write("Probably that this whole programming thing is witchcraft\r\n")
+    verify(out).write("We're not really worried about it to be honest.")
+    verify(out).write("\r\n")
+
+    verify(out).flush()
+    verify(out).close()
   }
 }
